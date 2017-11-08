7929cfa555eb7d983a8f32c031d4a1ff78408a12
Dat Vo <tiendatvo101095@gmail.com>
2017-11-08 14:55:04 +0700

    Initial Commit

---
+++ Eng-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Eng-App
+//
+//  Created by DXC-Technology on 11/8/17.
+//  Copyright © 2017 DXC-Technology. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
