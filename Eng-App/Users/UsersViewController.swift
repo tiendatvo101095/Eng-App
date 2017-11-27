bec2ad1642765127cf7182f063abdb797cf4572d
Lukas Nguyen <nguyenthanhhoai0107@gmail.com>
2017-11-27 08:44:53 +0700

    custom login view

--- Eng-App/Users/UsersViewController.swift
+++ Eng-App/Users/UsersViewController.swift
@@ -13,9 +13,10 @@ import FirebaseAuth
 class UsersViewController: UIViewController {
     
     @IBOutlet weak var viewBg: UIView!
-    @IBOutlet weak var createNewGameBtn: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
     @IBOutlet weak var usernameView: UITextField!
     @IBOutlet weak var passwordView: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
     
     override func viewDidLoad() {
@@ -29,6 +30,13 @@ class UsersViewController: UIViewController {
         usernameView.attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
         passwordView.borderStyle = .none
         passwordView.textColor = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+        
+        loginButton.backgroundColor = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
+        loginButton.setTitleColor(UIColor(red:0.15, green:0.72, blue:0.00, alpha:1.0), for: .normal)
+        loginButton.layer.cornerRadius = 20
+        
+        signupButton.setTitleColor(
+            UIColor(red:0.99, green:0.76, blue:0.00, alpha:1.0), for: .normal)
     }
 
     override func didReceiveMemoryWarning() {
--- Eng-App/Users/ViewController.swift
+++ Eng-App/Users/ViewController.swift
@@ -11,59 +11,16 @@ import Firebase
 import FirebaseAuth
 
 class ViewController: UIViewController {
-    @IBOutlet weak var userNameTextField: UITextField!
     @IBOutlet weak var viewBg: UIView!
-    @IBOutlet weak var gameNowBg: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UIGraphicsBeginImageContext(viewBg.frame.size)
-        UIImage(named: "app-bg.jpg")?.draw(in: viewBg.bounds)
-        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        viewBg.backgroundColor = UIColor(patternImage: image)
-        userNameTextField.contentVerticalAlignment = UIControlContentVerticalAlignment.center
-        gameNowBg.backgroundColor = UIColor(red:0.12, green:0.60, blue:1.00, alpha:1.0)
-        gameNowBg.layer.cornerRadius = 30
-        
-        userNameTextField.backgroundColor = UIColor(red:1.00, green:1.00, blue:1.00, alpha:1.0)
-        userNameTextField.layer.cornerRadius = 30
+        viewBg.backgroundColor = UIColor(red:0.15, green:0.72, blue:0.00, alpha:1.0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-        
-    }
-
-    @IBAction func StartButton(_ sender: Any) {
-        let email = userNameTextField.text!
-        Auth.auth().createUser(withEmail: email + "@gmail.com", password: email, completion: {(user:User?, error) in
-
-                var ref: DatabaseReference!
-                ref = Database.database().reference(fromURL: "https://fir-43245.firebaseio.com/")
-                guard let uid = user?.uid else{return}
-                
-                let userRef = ref.child("users").child(uid)
-                let value = ["name": email]
-
-                userRef.updateChildValues(value, withCompletionBlock: { (err, ref) in
-                    
-                    if let err = err {
-                        print(err)
-                        return
-                    }
-                    
-                    print("Saved user successfully into Firebase db")
-                    
-                })
-        })
-        
-    }
-    
 }
 
